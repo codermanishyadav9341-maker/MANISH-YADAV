fbd56419aa5fe44af008a76b47dba2d91cd721fa
codermanishyadav9341-maker <codermanishyadav9341@gmail.com>
2025-12-11 19:19:18 +0530

    Add files via upload

--- Class.java
+++ Class.java
@@ -1,53 +1,238 @@
-package CALCULATORS;
+package WRAPPER;
+
+//import java.util.Scanner;
+//
+//public class Class {
+//    public static void main(String[] args){
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//           System.out.print("Enter your number:- ");
+//           int num = scan.nextInt();
+//
+//           Integer ab = num;  /// Autoboxing
+//
+//           System.out.print("Wrapper Object:- " +ab);
+//
+//    }
+//}
+
+//import java.util.Scanner;
+//
+//public class Class{
+//    public static void main(String[] args){
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//          System.out.print("Enter your Wrapper Object:- ");
+//          Integer so = scan.nextInt();
+//
+//          int num = so;  //Unboxing ;
+//
+//          System.out.print("Primitive Value:- " +num);
+//    }
+//}
+//
+
+
+//import java.util.Scanner;
+//
+//public class Class{
+//    public static void main(String[] args){
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter your number");
+//         String so = scan.nextLine();
+//
+//         int num = Integer.parseInt(so);     // String Convert int to Primitive Data  Types;
+//
+//         System.out.print("Parsed Integer:- " +num);
+//    }
+//}
+
+
+//import java.util.Scanner;
+//
+//public class Class{
+//    public static void main(String[] args){
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter your number:- ");
+//        String num1 = scan.nextLine();
+//
+//        int num = Integer.parseInt(num1);
+//
+//        System.out.print("Parsed Value:- " +num);
+//
+//    }
+//}
+
+
+//import java.util.Scanner;
+//
+//public class Class{
+//    public static void main(String[] main){
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter your Number:- ");
+//        String str = scan.nextLine();
+//
+//        Integer num = Integer.valueOf(str);    //  String Convert in to Wrapper Object;
+//
+//        System.out.print("Wrapper Object:- " +num);
+//
+//    }
+//}
+
+
+//import java.util.Scanner;
+//
+//public class Class{
+//    public static void main(String[] args){
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter your first number:- ");
+//        Integer num1 = scan.nextInt();
+//
+//        System.out.print("Enter your Second number:- ");
+//        Integer num2 = scan.nextInt();
+//
+//        System.out.print("a = " +num1 +" \nb = " +num2);
+//    }
+//
+//}
+
+
+//import java.util.Scanner;
+//
+//public class Class{
+//    public static void main(String[] args){
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter your first number:- ");
+//        Integer num1 = scan.nextInt();
+//
+//        System.out.print("Enter your Second number:- ");
+//        Integer num2 = scan.nextInt();
+//
+//        if((num1 > num2) || (num2 > num1)){
+//            System.out.print("Maximum");
+//        }
+//         else{
+//             System.out.print("Minimum");
+//        }
+//    }
+//}
+
+
+//import java.util.Scanner;
+//
+//public class Class{
+//    public static void main(String[] args){
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter your number:- ");
+//        String num1 = scan.nextLine();
+//
+//        int num = Integer.parseInt(num1);
+//
+//        System.out.print(num);
+//    }
+//}
+
+//import java.util.Scanner;
+//
+//public class Class{
+//    public static void main(String[] args){
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter your Primitive Data Types:- ");
+//        int num1 = scan.nextInt();
+//
+//        Integer num = num1;
+//
+//        System.out.print("Wrapper Object is:- " +num);
+//
+//    }
+//}
+
+//import java.util.Scanner;
+//
+//public class Class{
+//    public static void main(String[] args){
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter your Wrapper Object:- ");
+//        Integer num = scan.nextInt();
+//
+//        int num1 = num;
+//
+//        System.out.print("Primitive Types:- " +num1);
+//    }
+//}
+
+
+//import java.util.Scanner;
+//
+//public class Class{
+//    public static void main(String[] args){
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter your number:- ");
+//        String num = scan.nextLine();
+//
+//        int num1 = Integer.parseInt(num);
+//
+//        System.out.print("Primitive Types:- " +num1);
+//    }
+//}
+
+//import java.util.Scanner;
+//
+//public class Class{
+//    public static void main(String[] args){
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter your number:- ");
+//        String num = scan.nextLine();
+//
+//        Integer num1 = Integer.valueOf(num);
+//
+//        System.out.print("Wrapper Object:- " +num1);
+//
+//    }
+//}
+
 
 import java.util.Scanner;
 
-public class Class {
-    public static void main(String[] args){
+public class Class{
+    public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter your first Number:- ");
-        int a = scan.nextInt();
+        System.out.print("Enter your first number:- ");
+        int num1 = scan.nextInt();
 
-        System.out.print("Enter your Second Number:- ");
-        int b = scan.nextInt();
+        System.out.print("Enter your double number:- ");
+        double num2 = scan.nextDouble();
 
-        System.out.print("Input:- +,-,*,/,%%:----");
-        char ch = scan.next().charAt(0);
+        double sum = num1 + num2;
 
-         switch(ch){
-
-                 case '+':
-                 int result = a+b;
-                 System.out.print("Sum of a and b is:- " +result);
-                 break;
-
-                 case '-':
-                 int sub = a-b;
-                 System.out.print("Subtract of a and b is:- " +sub);
-                 break;
-
-                 case '*':
-                 int multi = a*b;
-                 System.out.print("Product of a and b is:- " +multi);
-                 break;
-
-                 case '/':
-                 int divided = a/b;
-                 System.out.print("Divided of a and b is:- " +divided);
-                 break;
-
-             case '%':
-                 int remainder = a%b;
-                 System.out.println("Remainder of a and b is:- " +remainder);
-                 break;
+        System.out.print("Sum is:- " +sum);
 
 
-             default:
-                 System.out.print("Invalid input");
-                 break;
-
-         }
     }
+
+
 }
--- Exam.java
+++ Exam.java
@@ -1,46 +1,23 @@
-package PATTERN;
+package WRAPPER;
 
 import java.util.Scanner;
 
-//public class Exam{
-//    public static void main(String[] args){
-//
-//        Scanner scan = new Scanner(System.in);
-//
-//        System.out.print("Enter your number:- ");
-//        int num = scan.nextInt();
-//
-//        for(int i = 1; i<=num; i++){
-//            for(int j = 1; j<=num; j++){
-//                if((i+j)%2==0){
-//                    System.out.print("* ");
-//                }
-//                 else{
-//                     System.out.print("# ");
-//                }
-//            }
-//            System.out.println();
-//        }
-//    }
-//}
-
-
-// Questions no. 1;
-
-
-public class Exam{
+public class Exam {
     public static void main(String[] args){
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter your number:- ");
-        int num = scan.nextInt();
+        System.out.print("Enter your first number:- ");
+        Integer num1 = scan.nextInt();
 
-        for(int i = 1; i<=num; i++){
-            for(int j = 1; j<=num; j++){
-                System.out.print("* #");
-            }
-            System.out.println();
-        }
+        System.out.print("Enter your Second number:- ");
+        Integer num2 = scan.nextInt();
+
+        System.out.print("Enter your Third number:- ");
+        Integer num3 = scan.nextInt();
+
+        Integer max = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+
+        System.out.print("Maximum Number is:- " +max);
     }
 }
