37cd9cc2023272b258bed655128e3b1e3ca7978a
codermanishyadav9341-maker <codermanishyadav9341@gmail.com>
2025-12-11 19:16:17 +0530

    Add files via upload

--- Exam.java
+++ Exam.java
@@ -1,16 +1,46 @@
-package Import;
+package PATTERN;
 
-import INHERITANCE.Student;
+import java.util.Scanner;
+
+//public class Exam{
+//    public static void main(String[] args){
+//
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.print("Enter your number:- ");
+//        int num = scan.nextInt();
+//
+//        for(int i = 1; i<=num; i++){
+//            for(int j = 1; j<=num; j++){
+//                if((i+j)%2==0){
+//                    System.out.print("* ");
+//                }
+//                 else{
+//                     System.out.print("# ");
+//                }
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
 
 
-public class Exam {
+// Questions no. 1;
 
-    public static void main(String[] args) {
 
-        Student s = new Student();
+public class Exam{
+    public static void main(String[] args){
 
-        System.out.println("------------");
+        Scanner scan = new Scanner(System.in);
 
-        s.Student1();
+        System.out.print("Enter your number:- ");
+        int num = scan.nextInt();
+
+        for(int i = 1; i<=num; i++){
+            for(int j = 1; j<=num; j++){
+                System.out.print("* #");
+            }
+            System.out.println();
+        }
     }
 }
